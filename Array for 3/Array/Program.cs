daa85b68400f9280118bec8f05898e780b6aa467
kate <k-b-v@yandex.ru>
2022-11-13 19:22:01 +0300

    VScode and gitignore

---
+++ Array for 3/Array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+string[] array =  {"hello","2", "may", "1234"}; 
+string[] arrayNew  = new string[array.Length];
+
+void ArrayLengthIndexThree(string[] array, string[] arrayNew)
+{
+int size = 3;
+int index = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+if(array[i].Length <= size){
+  
+    arrayNew[index] = array[i];
+    index++;  
+}
+}
+}
+ void PrintArray(string[] arr)
+{
+    int count = arr.Length;
+    Console.Write("");
+     for (int i = 0; i < count; i++)
+     {
+        System.Console.Write($"{arr[i]} ");
+     }
+     System.Console.WriteLine("");
+}
+
+ArrayLengthIndexThree(array, arrayNew);
+PrintArray(arrayNew);
